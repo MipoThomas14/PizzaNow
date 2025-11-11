c1568b7fc3363b0ebaab428e35d3f5c1edb4d2d9
Mipo <Mipothomas14@gmail.com>
2025-11-11 15:46:09 -0500

    surpressed a warning by requiring transitive javafx.graphics for clients

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module edu.ksu.pizzanow {
   requires javafx.base;
-  requires javafx.graphics;
   requires javafx.controls;
   requires javafx.fxml;
+  requires transitive javafx.graphics;
 
   opens edu.ksu.pizzanow.ui to javafx.fxml;
   exports edu.ksu.pizzanow;
