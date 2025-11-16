b280db0278936fccb8c43f210eed68c2890fcb8d
Mipo <Mipothomas14@gmail.com>
2025-11-15 23:47:26 -0500

    removed fromCSV test

--- src/test/java/edu/ksu/domain/model/CustomerTest.java
+++ src/test/java/edu/ksu/domain/model/CustomerTest.java
@@ -1,9 +1,6 @@
 package edu.ksu.domain.model;
 
 import static org.junit.jupiter.api.Assertions.*;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
