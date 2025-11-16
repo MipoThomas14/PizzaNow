a8c7eb39c9143edb02e1131d8589b08636e4e620
Mipo <Mipothomas14@gmail.com>
2025-11-16 00:45:46 -0500

    Created Order class test file

--- src/test/java/edu/ksu/domain/model/OrderTest.java
+++ src/test/java/edu/ksu/domain/model/OrderTest.java
@@ -1,9 +1,9 @@
 package edu.ksu.domain.model;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
-import edu.ksu.pizzanow.domain.model.Customer;
 import edu.ksu.pizzanow.domain.model.Order;
+import edu.ksu.pizzanow.domain.model.Customer;
 import edu.ksu.pizzanow.domain.type.PaymentType;
 
 public class OrderTest {
