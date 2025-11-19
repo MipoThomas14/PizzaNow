d2289febf98826d78d4c4e536d299b5a7c23f73f
Mipo <Mipothomas14@gmail.com>
2025-11-19 12:09:27 -0500

    feat: created test derivatives of CustomerService and OrderService for testing purposes

---
+++ src/test/java/edu/ksu/service/TestCustomerService.java
@@ -0,0 +1,116 @@
+package edu.ksu.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import edu.ksu.pizzanow.data.FileHandler;
+import edu.ksu.pizzanow.domain.model.Customer;
+import edu.ksu.pizzanow.domain.type.PaymentType;
+import edu.ksu.pizzanow.service.CustomerService;
+
+
+public class TestCustomerService extends CustomerService {
+    private final FileHandler fileHandler;
+
+    public TestCustomerService(){
+        fileHandler = new FileHandler();
+    }
+
+
+    ////// PUBLIC API
+    @Override
+    public Customer createCustomer(String name, String phoneNumber, PaymentType paymentType) throws IOException {
+        Customer newCustomer = new Customer(name, phoneNumber, paymentType);
+        saveToFile(newCustomer);
+        return newCustomer;
+    }
+
+    @Override
+    public void saveCustomerData(Customer customer) throws IOException {
+        saveToFile(customer);
+    }
+
+    @Override
+    public boolean existsByPhone(String identifier) throws IOException{
+        boolean exists = false;
+        boolean isPhoneNumber = determineIdentifierType(identifier);
+
+        if(isPhoneNumber){
+            List<String[]> customerRows = fileHandler.readCSV("test-customers.csv", true);
+            for(String[] row : customerRows){
+                if(row.length < 2){
+                    continue;
+                }
+
+                String phoneNumber = row[0];
+                if(phoneNumber.equals(identifier)){
+                    exists = true;
+                    break;
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Identifier input is not a phone number.");
+        }
+
+        return exists;
+    }
+
+    @Override
+    public boolean existsByName(String identifier) throws IOException {
+        boolean exists = false;
+        boolean isString = !(determineIdentifierType(identifier));
+
+        if(isString){
+            List<String[]> customerRows = fileHandler.readCSV("test-customers.csv", true);
+            for(String[] row : customerRows){
+                if(row.length < 2){
+                    continue;
+                }
+                
+                String name = row[1];
+                if(name.equalsIgnoreCase(identifier)){
+                    exists = true;
+                    break;
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Identifier input is not a name.");
+        }
+
+        return exists;
+    }
+
+
+
+
+    ///////// PRIVATE METHODS
+    // returns true if phone number, else 
+    private void saveToFile(Customer customer) throws IOException{
+        String[] toSave = {
+            customer.getPhoneNumber(), // phoneNumber
+            customer.getName(), // name
+            customer.getPaymentType().toString() // paymentType
+        };
+
+        try {
+            fileHandler.updateRow("test-customers.csv", customer.getPhoneNumber(), toSave);
+        } catch (IllegalArgumentException e) {
+            fileHandler.appendRow("test-customers.csv", toSave);
+        }
+    }
+
+    private boolean determineIdentifierType(String identifier){
+        boolean isPhoneNumber = false;
+
+        if(identifier == null || identifier.isBlank()){
+            return false;
+        }
+
+        try {
+            Integer.valueOf(identifier);
+            isPhoneNumber = true;
+        } catch (NumberFormatException e) {}
+
+        return isPhoneNumber;
+    }
+}
---
+++ src/test/java/edu/ksu/service/TestOrderService.java
@@ -0,0 +1,58 @@
+package edu.ksu.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import edu.ksu.pizzanow.data.FileHandler;
+import edu.ksu.pizzanow.domain.model.Customer;
+import edu.ksu.pizzanow.domain.model.Item;
+import edu.ksu.pizzanow.domain.model.Order;
+import edu.ksu.pizzanow.domain.type.OrderType;
+import edu.ksu.pizzanow.service.OrderService;
+
+
+public class TestOrderService extends OrderService {
+    private final FileHandler fileHandler;
+
+    public TestOrderService(){
+        fileHandler = new FileHandler();
+    }
+    
+    @Override
+    public Order createOrder(Customer customer, List<Item> orderItems, OrderType orderType) {
+        Order newOrder = new Order(customer, orderItems);
+        newOrder.setOrderType(orderType);
+
+        return newOrder;
+    }
+
+    @Override
+    public void finalizeOrder(Order order) throws IOException{
+        order.setOrderStatus();
+        order.setTimeCreated();
+        saveToFile(order);
+    }
+
+    private void saveToFile(Order toSave) throws IOException{
+        Customer customer = toSave.getCustomer();
+
+        String[] orderLine = {
+            toSave.getOrderId(), // orderId
+            customer.getPhoneNumber(), // customerPhone
+            toSave.getOrderType().toString(), // orderType
+            toSave.getPaymentType().toString(), // paymentType
+            String.valueOf(toSave.getSubTotal()), // subtotal
+            String.valueOf(toSave.getTax()), // tax
+            String.valueOf(toSave.getTotal()), // total
+            toSave.getTimeCreated().toString(), // timeCreated
+            toSave.getOrderStatus().toString() // orderStatus
+        };
+
+        try {
+            fileHandler.updateRow("test-orders.csv", toSave.getOrderId(), orderLine);
+        } catch (IllegalArgumentException e) {
+            fileHandler.appendRow("test-orders.csv", orderLine);
+        }
+    }
+
+}
